479de5a3e3ee08836e887da09ed3adf3d021a85a
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-05-07 14:04:53 +1000

    Added tryGet method.

--- test/Database.js
+++ test/Database.js
@@ -1,6 +1,8 @@
 import sinon from 'sinon';
 import proxyquire from 'proxyquire';
 
+import databaseResults from '../lib/databaseResults';
+
 describe('Database document', function () {
 
   let _nano, _db, _database;
@@ -172,6 +174,57 @@ describe('Database document', function () {
         e.should.equal(err);
         done();
       });
+
+      _callback(err);
+    });
+  });
+
+  describe('tryGet', () => {
+
+    let _docName, _params, _promise, _callback;
+
+    beforeEach(() => {
+      _docName = 'my-document';
+      _params = 'params';
+      _promise = _database.tryGet(_docName, _params);
+      _callback = _db.get.getCall(0).args[2];
+    });
+
+    it('should call get with correct arguments', () => {
+      _db.get.calledWith(_docName, _params).should.be.true;
+    });
+
+    it('should return the document on success', done => {
+      const body = {};
+
+      _promise.then(b => {
+        b.should.equal(body);
+        done();
+      });
+
+      _callback(undefined, body);
+    });
+
+    it('should return not-found status if document is not found', done => {
+      const err = new Error('deleted');
+
+      _promise.then(d => {
+        d.should.equal(databaseResults.notFound);
+        done();
+      })
+      .done();
+
+      _callback(err);
+    });
+
+    it('should throw an exception for other errors', done => {
+      const err = new Error('real bummer');
+
+      _promise.catch(e => {
+        e.should.equal(err);
+        done();
+      })
+      .done();
 
       _callback(err);
     });
